f1f46f08c01212cfa97b6ef0598c7c3d14276811
GabrielDragone <gabriel.dragone@live.com>
2022-07-13 14:18:44 -0300

    Criação de outros componentes, junto com o estilo css de cada um

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
 // Quando houver mais de um componente dentro do return, inseri-los dentro do fragment <> </>
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
