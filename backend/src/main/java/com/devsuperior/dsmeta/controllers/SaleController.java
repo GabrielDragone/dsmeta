5a710a3ba412d70dfc0d1ff0d80ed48e0c4caf38
GabrielDragone <gabriel.dragone@live.com>
2022-07-14 14:26:57 -0300

    Adicionada configuração e dependencia da biblioteca da Twilio para envio de SMS. Implementada SmsService com exemplo da Twilio, mas também aproveitei para criar um método com validações diferentes do método do processor. Criada dois enpoints de envio de SMS, um para teste e outro que busca uma venda passada via PathVariable

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,15 +1,14 @@
-package com.devsuperior.dsmeta;
+package com.devsuperior.dsmeta.controllers;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 // É a classe que implementa a nossa API, ou seja, nossos endpoints. Para onde o front vai apontar para fazer as requisições pro back:
@@ -20,18 +19,30 @@ public class SaleController {
     @Autowired
     private SaleService saleService;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping // Verbo GET. Responde via web usando HTTP protocolo WEB. Responde conteúdo json:
     public List<Sale> findSales() {
         return saleService.findSales();
     }
 
-    @GetMapping() // Buscar por data e de forma paginada, ou seja, não retornará mais todos os dados, e sim apenas um trecho:
-    @RequestMapping(value = "/byDate")
+    @GetMapping("/byDate") // Buscar por data e de forma paginada, ou seja, não retornará mais todos os dados, e sim apenas um trecho:
     public Page<Sale> findSalesPaginatedByDate(
             @RequestParam(value = "minDate", defaultValue = "") String minDate, // Parâmetros que devem ser enviados para o front. Em caso de não preenchimento, valor padrão será String vazia
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return saleService.findSalesPaginatedByDate(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/notifySms")
+    public void notifySms() {
+        smsService.sendSms();
+    }
+
+    @GetMapping("{id}/notifySms") // Quando inserimos o parametro dessa forma, ele vira um PathVariable e deve ter o mesmo nome do parametro no método abaixo:
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSaleBySms(id);
     }
 
 }
---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,85 @@
+package com.devsuperior.dsmeta.services;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service // Registra a classe como component Service.
+public class SmsService {
+
+    @Value("${twilio.sid}") // Annotation que busca a variável de ambiente definida no application.properties.
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    SaleRepository saleRepository;
+
+    // Modelo de teste da Documentação da Twilio:
+    public void sendSms() {
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, "Teste").create();
+
+        System.out.println("Mensagem enviada com sucesso. Id:" + message.getSid());
+    }
+
+    // Busca uma venda por id e a envia por SMS:
+    public void sendSaleBySms(Long saleId) {
+
+        // O Optional nos ajuda a evitar NullPointerExceptions. Verificar o próximo if:
+        Optional<Sale> sale = saleRepository.findById(saleId); // Busca a venda por ID.
+
+        StringBuilder msg = new StringBuilder();
+
+        // Aqui validamos se o sale está presente, ou seja, se foi encontrado alguma valor:
+        if (sale.isPresent()) {
+            // Exemplo de concatenação com String:
+            String date = sale.get().getDate().getDayOfMonth() + "/" + sale.get().getDate().getMonthValue()
+                    + "/" + sale.get().getDate().getYear();
+            String amount = String.format("%.2f", sale.get().getAmount());
+
+            // Utilizei StringBuilder pois quando concatenamos novas string para o mesmo objeto não são criadas cópias dos
+            // objetos como é realizado no métoco concat da classe String (verificar exemplo de date). Isso contribui para
+            // um melhor desempenho do sistema:
+            msg.append("A venda ")
+                    .append(saleId)
+                    .append(" foi realizada pelo vendedor(a) ")
+                    .append(sale.get().getSellerName())
+                    .append(" na data de ")
+                    .append(date)
+                    .append(" no total de R$")
+                    .append(amount);
+        } else {
+            msg.append("A compra ")
+                    .append(saleId)
+                    .append(" não foi encontrada :( Tente novamente com outra ID!");
+        }
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg.toString()).create();
+
+        System.out.println("Mensagem enviada com sucesso. Id:" + message.getSid());
+    }
+}
